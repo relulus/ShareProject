8ec0568d630add479405e25439fd484eee977df2
relulus <relulus@users.noreply.github.com>
2024-12-01 19:38:16 +0200

    Update COntext

--- Entities/ApiPhantomContext.cs
+++ Entities/ApiPhantomContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
 using System.Collections.Generic;
 using System.Text.Json;
 using ApiPhantom.Models;
@@ -16,7 +17,7 @@ namespace ApiPhantom.Data
         {
         }
 
-        // DbSet properties for all entities
+        // DbSet properties for the entities
         public DbSet<ServiceCatalog> ServiceCatalogs { get; set; }
         public DbSet<ApiCatalog> ApiCatalogs { get; set; }
         public DbSet<RequestResponse> RequestResponses { get; set; }
@@ -27,7 +28,11 @@ namespace ApiPhantom.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Configure enum InterceptionMode to be stored as string
+            // Configure enum HTTPMethod and InterceptionMode to be stored as strings
+            modelBuilder.Entity<ApiCatalog>()
+                .Property(a => a.Method)
+                .HasConversion<string>();
+
             modelBuilder.Entity<ServiceCatalog>()
                 .Property(s => s.InterceptionMode)
                 .HasConversion<string>();
@@ -36,60 +41,43 @@ namespace ApiPhantom.Data
                 .Property(a => a.InterceptionMode)
                 .HasConversion<string>();
 
-            modelBuilder.Entity<InterceptedService>()
-                .Property(is => is.InterceptionMode)
-                .HasConversion<string>();
-
             modelBuilder.Entity<InterceptedApi>()
                 .Property(ia => ia.InterceptionMode)
                 .HasConversion<string>();
 
-            // ServiceCatalog → ApiCatalog (One-to-Many)
+            modelBuilder.Entity<InterceptedService>()
+                .Property(is => is.InterceptionMode)
+                .HasConversion<string>();
+
+            // Configure relationships
             modelBuilder.Entity<ServiceCatalog>()
                 .HasMany(sc => sc.ApiCatalogs)
                 .WithOne(ac => ac.ServiceCatalog)
                 .HasForeignKey(ac => ac.ServiceCatalogId);
 
-            // ApiCatalog → RequestResponse (One-to-Many)
             modelBuilder.Entity<ApiCatalog>()
                 .HasMany(ac => ac.RequestResponses)
                 .WithOne(rr => rr.ApiCatalog)
                 .HasForeignKey(rr => rr.ApiCatalogId);
 
-            // ServiceCatalog → InterceptedService (One-to-One)
-            modelBuilder.Entity<ServiceCatalog>()
-                .HasOne(sc => sc.InterceptedService)
-                .WithOne(is => is.ServiceCatalog)
-                .HasForeignKey<InterceptedService>(is => is.ServiceCatalogId);
-
-            // InterceptedService → InterceptedApi (One-to-Many)
             modelBuilder.Entity<InterceptedService>()
                 .HasMany(is => is.InterceptedApis)
                 .WithOne(ia => ia.InterceptedService)
                 .HasForeignKey(ia => ia.InterceptedServiceId);
 
-            // ApiCatalog → InterceptedApi (One-to-One)
-            modelBuilder.Entity<ApiCatalog>()
-                .HasOne(ac => ac.InterceptedApi)
-                .WithOne(ia => ia.ApiCatalog)
-                .HasForeignKey<InterceptedApi>(ia => ia.ApiCatalogId);
-
-            // InterceptedApi → RequestResponse (One-to-Many)
             modelBuilder.Entity<InterceptedApi>()
                 .HasMany(ia => ia.RequestResponses)
                 .WithOne(rr => rr.InterceptedApi)
                 .HasForeignKey(rr => rr.InterceptedApiId);
 
-            // Dataset → DatasetRequestResponse (One-to-Many)
             modelBuilder.Entity<Dataset>()
                 .HasMany(d => d.DatasetRequestResponses)
                 .WithOne(drr => drr.Dataset)
                 .HasForeignKey(drr => drr.DatasetId);
 
-            // RequestResponse → DatasetRequestResponse (One-to-Many)
-            modelBuilder.Entity<RequestResponse>()
-                .HasMany(rr => rr.DatasetRequestResponses)
-                .WithOne(drr => drr.RequestResponse)
+            modelBuilder.Entity<DatasetRequestResponse>()
+                .HasOne(drr => drr.RequestResponse)
+                .WithMany(rr => rr.DatasetRequestResponses)
                 .HasForeignKey(drr => drr.RequestResponseId);
 
             // Configure JSON serialization for dictionary properties in RequestResponse
@@ -109,7 +97,7 @@ namespace ApiPhantom.Data
                 .Property(rr => rr.ResponseHeaders)
                 .HasConversion(dictionaryConverter);
 
-            // Set default values for IsActive and timestamps
+            // Default values
             modelBuilder.Entity<ServiceCatalog>()
                 .Property(s => s.IsActive)
                 .HasDefaultValue(true);
