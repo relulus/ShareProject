9b0f38937123197cf08526f0ce963718a88f3337
relulus <relulus@users.noreply.github.com>
2024-12-02 16:55:15 +0200

    controller

---
+++ Service/CatalogService.cs
@@ -0,0 +1,43 @@
+﻿using ApiPhantom.DAL;
+using ApiPhantom.Models;
+using ApiPhantom.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPhantom.Service
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly ApiPhantomContext _context;
+
+        public CatalogService(ApiPhantomContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ServiceCatalog>> GetAllServicesAsync()
+        {
+            return await _context.ServiceCatalog
+                .Select(s => new ServiceCatalog
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    Description = s.Description,
+                    BaseUrl = s.BaseUrl
+                })
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<ApiCatalog>> GetApisByServiceIdAsync(Guid serviceId)
+        {
+            var service = await _context.ServiceCatalog
+                .Include(s => s.ApiCatalogs)
+                .FirstOrDefaultAsync(s => s.Id == serviceId);
+
+            return service?.ApiCatalogs ?? Enumerable.Empty<ApiCatalog>();
+        }
+    }
+}
---
+++ Service/ICatalogService.cs
@@ -0,0 +1,13 @@
+﻿using ApiPhantom.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiPhantom.Service.Interfaces
+{
+    public interface ICatalogService
+    {
+        Task<IEnumerable<ServiceCatalog>> GetAllServicesAsync();
+        Task<IEnumerable<ApiCatalog>> GetApisByServiceIdAsync(Guid serviceId);
+    }
+}
